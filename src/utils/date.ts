74047ce01711aa87145fdb8b62cf25d1bd3c6169
Alex-Zamora <wixarikalex@gmail.com>
2025-08-28 18:41:22 -0600

    feat: add utility to calculate expiration date

---
+++ src/utils/date.ts
@@ -0,0 +1,25 @@
+import { add } from 'date-fns';
+
+export const calculateExpirationDate = (expiresIn: string = '15m'): Date => {
+    // Match number + unit (m = minutes, h = hours, d = days)
+    const match = expiresIn.match(/^(\d+)([mhd])$/);
+    if (!match) throw new Error('Invalid format. Use "15m", "1h", or "2d".');
+
+    const [, value, unit] = match,
+        expirationDate = new Date();
+
+    switch (unit) {
+        case 'm': // minutes
+            return add(expirationDate, { minutes: parseInt(value) });
+        case 'h': // hours
+            return add(expirationDate, { hours: parseInt(value) });
+        case 'd': // days
+            return add(expirationDate, { days: parseInt(value) });
+        default:
+            throw new Error('Invalid unit. Use "m", "h", or "d".');
+    }
+};
+
+// export const thirtyDaysFromNow = () => new Date(Date.now() + 5 * 24 * 60 * 60 * 1000);
+
+const thirtyDaysFromNow = () => add(new Date(), { days: 30 });
