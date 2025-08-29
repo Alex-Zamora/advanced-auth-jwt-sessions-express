9c469fb1797d33f90dd58f829286809fa6be10f7
Alex-Zamora <wixarikalex@gmail.com>
2025-08-28 18:41:05 -0600

    feat: add catchErrors utility for handling async controller errors

---
+++ src/utils/catchError.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+
+type AsyncController = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => Promise<Response | void>;
+
+export const catchErrors =
+    (controller: AsyncController): AsyncController =>
+    async (req, res, next) => {
+        try {
+            await controller(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    };
