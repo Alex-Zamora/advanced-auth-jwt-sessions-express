97a2ea2521b3326ef1dbde6963a739ea0a967bf0
Alex-Zamora <wixarikalex@gmail.com>
2025-08-30 01:22:11 -0600

    refactor: create and revoke session in auth services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,9 +5,26 @@ import { User } from '@models/User.model';
 import Session from '@models/Session.model';
 import { AppError } from '@utils/AppError';
 import { signJWT, verifyJWT } from '@utils/jwt';
+import { calculateExpirationDate, ONE_DAY_IN_MS } from '@utils/date';
 import { clearAuthCookies } from '@utils/cookies';
 
-export const registerService = async (name: string, email: string, password: string) => {
+interface RegisterPayload {
+    name: string;
+    email: string;
+    password: string;
+    userAgent?: string;
+    ipAddress?: string;
+}
+
+type LoginPayload = Omit<RegisterPayload, 'name'>;
+
+export const registerService = async ({
+    name,
+    email,
+    password,
+    userAgent,
+    ipAddress,
+}: RegisterPayload) => {
     const user = await User.exists({ email });
 
     if (user)
@@ -19,9 +36,7 @@ export const registerService = async (name: string, email: string, password: str
     const newUser = await User.create({ name, email, password }),
         userId = newUser._id.toString();
 
-    // Validate if in register flow is good idea atenticate to the user.
-    // or afeter register form, show a otp form to validate email.
-    const session = await Session.create({ userId: user._id }),
+    const session = await Session.create({ userId, userAgent, ipAddress }),
         sessionId = session._id.toString();
 
     const accessToken = signJWT(
@@ -38,7 +53,7 @@ export const registerService = async (name: string, email: string, password: str
     return { user: newUser, accessToken, refreshToken };
 };
 
-export const loginService = async (email: string, password: string) => {
+export const loginService = async ({ email, password, userAgent, ipAddress }: LoginPayload) => {
     const user = await User.findOne({ email });
 
     if (!user)
@@ -54,9 +69,8 @@ export const loginService = async (email: string, password: string) => {
             type: 'simple',
         });
 
-    const session = await Session.create({ userId: user._id });
-
     const userId = user._id.toString(),
+        session = await Session.create({ userId, userAgent, ipAddress }),
         sessionId = session._id.toString(),
         accessToken = signJWT(
             'accessPrivateKey',
@@ -73,26 +87,53 @@ export const loginService = async (email: string, password: string) => {
 };
 
 export const refreshService = async (token: string, res: Response) => {
-    const { id } = await verifyJWT('refreshPublicKey', token);
+    const { sessionId } = await verifyJWT('refreshPublicKey', token);
 
-    if (!id) throw AppError.unauthorized({ message: 'Invalid refresh token' });
+    if (!sessionId) throw AppError.unauthorized({ message: 'Invalid refresh token' });
 
-    const user = await User.findById(id);
+    const session = await Session.findById({ _id: sessionId }),
+        DATE_NOW = Date.now();
+
+    if (!session) throw AppError.unauthorized({ message: 'Session does not exist' });
+
+    // another option:
+    // session.expiresAt <= new Date() using dates
+    if (session.expiresAt.getTime() <= DATE_NOW)
+        throw AppError.unauthorized({ message: 'Session expired' });
+
+    const user = await User.findById({ _id: session.userId });
     if (!user) {
+        await Session.findByIdAndDelete(session._id);
         clearAuthCookies(res);
         throw AppError.unauthorized({ message: 'User not found or deactivated' });
     }
 
-    const accessToken = signJWT(
-            'accessPrivateKey',
-            { id },
-            { expiresIn: config.JWT_ACCESS_EXPIRES_IN as StringValue },
-        ),
-        refreshToken = signJWT(
-            'refreshPrivateKey',
-            { id },
-            { expiresIn: config.JWT_REFRESH_EXPIRES_IN as StringValue },
-        );
+    // another option:
+    // session.expiresAt.getTime() using miliseconds
+    // new Date().getTime() using miliseconds
+    /**
+     * const timestamp1 = Date.now();           // 1693425600000
+     * const timestamp2 = new Date().getTime(); // 1693425600000
+     */
+    const sessionRequireRefresh = session.expiresAt.getTime() - DATE_NOW <= ONE_DAY_IN_MS;
+    if (sessionRequireRefresh) {
+        session.expiresAt = calculateExpirationDate(config.JWT_REFRESH_EXPIRES_IN);
+        await session.save();
+    }
 
-    return { accessToken, refreshToken };
+    const newRefreshToken = sessionRequireRefresh
+        ? signJWT(
+              'refreshPrivateKey',
+              { sessionId },
+              { expiresIn: config.JWT_REFRESH_EXPIRES_IN as StringValue },
+          )
+        : undefined;
+
+    const accessToken = signJWT(
+        'accessPrivateKey',
+        { userId: session.userId.toString(), sessionId: session._id.toString() },
+        { expiresIn: config.JWT_ACCESS_EXPIRES_IN as StringValue },
+    );
+
+    return { accessToken, newRefreshToken };
 };
