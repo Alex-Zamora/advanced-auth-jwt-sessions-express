4064953fe7b8badd47192926aff8486ed8383911
Alex-Zamora <wixarikalex@gmail.com>
2025-08-30 01:19:32 -0600

    refactor: accept userAgent annd ipAddress in auth controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from 'express';
 import { loginService, refreshService, registerService } from '@services/auth.service';
-import { setAuthCookies } from '@utils/cookies';
+import { getAccessTokenOptions, getRefreshTokenOptions, setAuthCookies } from '@utils/cookies';
 import { catchErrors } from '@utils/catchError';
 import { AppError } from '@utils/AppError';
 
 export const register = catchErrors(async (req: Request, res: Response) => {
     const { name, email, password } = req.body,
-        { user, refreshToken, accessToken } = await registerService(name, email, password);
+        body = {
+            name,
+            email,
+            password,
+            userAgent: req.headers['user-agent'],
+            ipAddress: req.ip,
+        },
+        { user, refreshToken, accessToken } = await registerService(body);
 
     return setAuthCookies({
         res,
@@ -24,7 +31,13 @@ export const register = catchErrors(async (req: Request, res: Response) => {
 
 export const login = catchErrors(async (req: Request, res: Response) => {
     const { email, password } = req.body,
-        { user, accessToken, refreshToken } = await loginService(email, password);
+        body = {
+            email,
+            password,
+            userAgent: req.headers['user-agent'],
+            ipAddress: req.ip,
+        },
+        { user, accessToken, refreshToken } = await loginService(body);
 
     return setAuthCookies({
         res,
@@ -49,13 +62,11 @@ export const refreshToken = catchErrors(async (req: Request, res: Response) => {
             detail: 'Missing refresh token',
         });
 
-    const { accessToken, refreshToken } = await refreshService(token, res);
+    const { accessToken, newRefreshToken } = await refreshService(token, res);
 
-    return setAuthCookies({
-        res,
-        accessToken,
-        refreshToken,
-    })
-        .status(200)
-        .json({ message: 'Tokens successfully updated' });
+    if (newRefreshToken) res.cookie('refreshToken', refreshToken, getRefreshTokenOptions());
+
+    return res.status(200).cookie('accessToken', accessToken, getAccessTokenOptions()).json({
+        message: 'Refresh token successfully updated',
+    });
 });
